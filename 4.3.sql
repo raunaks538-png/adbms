61f984570e551692f3b9424ae079843ddb19b927
raunaks538-png <raunaks538@gmail.com>
2025-09-24 10:46:57 +0530

    Create 4.3.sql

---
+++ 4.3.sql
@@ -0,0 +1,22 @@
+
+DROP TABLE IF EXISTS StudentEnrollments;
+
+CREATE TABLE StudentEnrollments (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(100) NOT NULL,
+    course_id VARCHAR(10) NOT NULL,
+    enrollment_date DATE NOT NULL
+);
+
+
+INSERT INTO StudentEnrollments (student_id, student_name, course_id, enrollment_date)
+VALUES
+(1, 'Ashish', 'CSE101', '2024-06-01'),
+(2, 'Smaran', 'CSE102', '2024-06-01'),
+(3, 'Vaibhav', 'CSE103', '2024-06-01');
+
+SELECT 'Initial Table State' AS Stage;
+SELECT * FROM StudentEnrollments;
+
+SELECT 'Final Table State' AS Stage;
+SELECT * FROM StudentEnrollments;
